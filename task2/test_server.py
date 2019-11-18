2ea931fb2d0fa08ed88099ce0ff56bf9b949a060
Inf1delis <v.mamedov@g.nsu.ru>
2019-11-19 00:06:46 +0700

    Updated server tests

--- task2/test_server.py
+++ task2/test_server.py
@@ -25,6 +25,26 @@ class BasicTests(unittest.TestCase):
         response = self.app.delete('/cache/1', follow_redirects=True)
         self.assertEqual(response.status_code, 204)
 
+    def test_full_flow(self):
+        key = self._k
+        value = self._value
+
+        response = self.app.get('/cache/' + key, follow_redirect=True)
+        self.assertEqual(response.status_code, 404)
+
+        response = self.app.put('/cache/' + key, data=value, follow_redirect=True)
+        self.assertEqual(response.status_code, 201)
+
+        response = self.app.get('/cache/' + key, follow_redirect=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(value, str(response.get_data().decode("utf-8").strip()))
+
+        response = self.app.delete('/cache/' + key, follow_redirect=True)
+        self.assertEqual(response.status_code, 204)
+
+        response = self.app.get('/cache/' + key, follow_redirect=True)
+        self.assertEqual(response.status_code, 404)
+
 
 if __name__ == '__main__':
     unittest.main()
